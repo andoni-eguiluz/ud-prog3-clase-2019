043a83fefbcb80ab769de3baca62916cd39b5513
Andoni Eguíluz <andoni.eguiluz@deusto.es>
2019-09-26 09:42:23 +0200

    Añadidos comentarios explicativos

--- Clase201920/src/es/deusto/prog3/cap00/resueltos/VentanaConfirmacionLenta.java
+++ Clase201920/src/es/deusto/prog3/cap00/resueltos/VentanaConfirmacionLenta.java
@@ -1,6 +1,7 @@
 package es.deusto.prog3.cap00.resueltos;
 
 import java.util.Random;
+import java.util.Set;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -25,30 +26,32 @@ public class VentanaConfirmacionLenta {
 	private static void procesoConfirmar() {
 		try {
 			Thread.sleep( 5000 + 1000*r.nextInt(6) );
-		} catch (InterruptedException e) {}
+		} catch (InterruptedException e) {
+			System.out.println( "Interrumpido" );
+		}
 	}
 	
 	public static void main(String[] args) {
-		// TODO Desarrollar la clase de acuerdo a los comentarios de la cabecera
-		JFrame v = new JFrame();
+		JFrame v = new JFrame();  // Se pueden crear ventanas directamente desde las clases de Swing (aunque es deseable cuando son un poco elaboradas hacerlas con clases nuevas que extiendan JFrame)
 		v.setSize( 600, 400 );
-		v.setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
+		v.setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );  // Si no se hace esto ¿qué pasa? Problema de la parada de Swing - todas las ventanas tienen que ser liberadas (dispose)
 		JTextField tfDatos = new JTextField( 20 );
-		v.add( tfDatos, BorderLayout.NORTH );
+		v.add( tfDatos, BorderLayout.NORTH );  // Si no se indica segundo parámetro, se añade al centro - porque el layout determina el añadido y el layout por defecto es BorderLayout
 		JButton bAceptar = new JButton( "Aceptar" );
-		v.add( bAceptar, BorderLayout.SOUTH );
-		v.setVisible( true );
-		bAceptar.addActionListener( new ActionListener() {
+		v.add( bAceptar, BorderLayout.SOUTH );  // Ojo que si no se indica el segundo parámetro se añade al centro, sustituyendo a cualquier cosa que hubiera en el centro
+		v.setVisible( true );  // Esto activa la ventana y lanza el hilo de Swing
+		bAceptar.addActionListener( new ActionListener() { // Clase anónima que implementa ActionListener para crear el objeto escuchador 
+			// Este objeto escuchador es un "paquete" de trabajo que se le manda a Swing pero que no se ejecuta... será Swing quien lo ejecute
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				Thread hilo = new Thread() {
-					public void run() {
-						System.out.println( "Empiezo" );
+				Thread hilo = new Thread() {  // Clase anónima heredada de Thread para crear el hilo
+					public void run() {  // Método principal que debe tener todo hilo - es como el main para cada hilo
+						System.out.println( "Empiezo" );  // Para ver cuándo empieza en consola
 						procesoConfirmar();
-						System.out.println( "Acabo" );
+						System.out.println( "Acabo" );  // Para ver cuándo acaba en consola
 					}
 				};
-				hilo.start();
+				hilo.start();  // hilo.run(); ejecutaría desde el mismo hilo (Swing) - no solucionaría nada
 			}
 		} );
 	}
