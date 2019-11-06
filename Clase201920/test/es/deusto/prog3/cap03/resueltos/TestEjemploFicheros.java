6f35f5a39538ab888a6043af74b38fd80987a63d
Andoni Eguíluz <andoni.eguiluz@deusto.es>
2019-11-06 18:39:32 +0100

    Hacks a EjemploFicheros

---
+++ Clase201920/test/es/deusto/prog3/cap03/resueltos/TestEjemploFicheros.java
@@ -0,0 +1,85 @@
+package es.deusto.prog3.cap03.resueltos;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import es.deusto.prog3.cap03.EjemploFicheros;
+import es.deusto.prog3.cap03.TipoUsuario;
+import es.deusto.prog3.cap03.Usuario;
+
+public class TestEjemploFicheros {
+
+	private ArrayList<Usuario> miListaUsuarios;
+	
+	// Crea una lista de 4 usuarios de ejemplo para los tests
+	@Before
+	public void setUp() throws Exception {
+		File carp = new File( "test/res" ); // Carpeta de recursos para prueba
+		carp.mkdirs(); // Crea la carpeta si no existe
+		miListaUsuarios = new ArrayList<Usuario>();
+		miListaUsuarios.add( new Usuario( "buzz", "#9abbf", "Buzz", "Lightyear", 101202303, TipoUsuario.Admin, "buzz@gmail.com", "amigo.de.woody@gmail.com" ) );
+		miListaUsuarios.add( new Usuario( "woody", "woody", "Woody", "The cowboy", 666111222, TipoUsuario.Cliente, "woody-toystory@gmail.com" ) );
+		miListaUsuarios.add( new Usuario( "pcruz", "4556gght", "Penélope", "Cruz", 609634852, TipoUsuario.Invitado, "penelope.cruz@gmail.com" ) );
+		miListaUsuarios.add( new Usuario( "jbardem", "bardemthebest", "Javier", "Bardem", 607349552, TipoUsuario.Invitado, "javier.bardem@gmail.com" ) );
+		// Fallo para el test de comas: meter una coma en un campo
+		// miListaUsuarios.add( new Usuario( "buzz2", "#9a,bbf", "Buzz", "Lightyear", 101202303, TipoUsuario.Admin, "buzz@gmail.com", "amigo.de.woody@gmail.com" ) );
+		// Fallo para el test de tags: 
+		// miListaUsuarios.add( new Usuario( "buzz2", "#9a\nbbf", "Buzz", "Lightyear", 101202303, TipoUsuario.Admin, "buzz@gmail.com", "amigo.de.woody@gmail.com" ) );
+		// Fallo para el test de bd:
+		// miListaUsuarios.add( new Usuario( "buzz2", "#9'abbf", "Buzz", "Lightyear", 101202303, TipoUsuario.Admin, "buzz@gmail.com", "amigo.de.woody@gmail.com" ) );
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		miListaUsuarios.clear();
+	}
+
+	@Test
+	public void testLeerYEscribirAFicheroConTags() {
+		EjemploFicheros.escribirAFicheroConTags( "test/res/test-usuarios-tags.txt", miListaUsuarios );
+		ArrayList<Usuario> l = EjemploFicheros.leerDeFicheroConTags( "test/res/test-usuarios-tags.txt" );
+		assertEquals( miListaUsuarios.size(), l.size() );
+		assertEquals( miListaUsuarios, l );  // Las listas deben ser iguales
+		l.clear();
+	}
+
+	@Test
+	public void testLeerYEscribirAFicheroConComas() {
+		EjemploFicheros.escribirAFicheroConComas( "test/res/test-usuarios-comas.txt", miListaUsuarios );
+		ArrayList<Usuario> l = EjemploFicheros.leerDeFicheroConComas( "test/res/test-usuarios-comas.txt" );
+		assertEquals( miListaUsuarios.size(), l.size() );
+		assertEquals( miListaUsuarios, l );  // Las listas deben ser iguales
+		l.clear();
+	}
+
+	@Test
+	public void testLeerYEscribirAFicheroSerializado() {
+		EjemploFicheros.escribirAFicheroSerializado( "test/res/test-usuarios.dat", miListaUsuarios );
+		ArrayList<Usuario> l = EjemploFicheros.leerDeFicheroSerializado( "test/res/test-usuarios.dat" );
+		assertEquals( miListaUsuarios.size(), l.size() );
+		assertEquals( miListaUsuarios, l );  // Las listas deben ser iguales
+		l.clear();
+	}
+
+	@Test
+	public void testLeerYEscribirABD() {
+		int numInserts = EjemploFicheros.escribirABD( "usuarios.bd", miListaUsuarios );
+		assertEquals( miListaUsuarios.size(), numInserts );
+		// Cogemos todos (sin where)
+		ArrayList<Usuario> alu = EjemploFicheros.leerDeBD( "usuarios.bd", "" );
+		assertEquals( miListaUsuarios, alu );  // Las listas deben ser iguales
+		// Cogemos solo uno (buzz)
+		alu = EjemploFicheros.leerDeBD( "usuarios.bd", "nick='buzz'" );
+		assertEquals( 1, alu.size() );  // Las listas deben ser iguales
+		assertEquals( miListaUsuarios.get(0), alu.get(0) );  // Las listas deben ser iguales
+		// Cogemos solo ninguno
+		alu = EjemploFicheros.leerDeBD( "usuarios.bd", "nick='no existo'" );
+		assertEquals( 0, alu.size() );  // Las listas deben ser iguales
+	}
+}
