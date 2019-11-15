2d6cc23c462305d4d3ab9151f5c4d2bebedffeaa
Andoni Eguíluz <andoni.eguiluz@deusto.es>
2019-11-15 13:04:26 +0100

    Recursividad en clase

--- Clase201920/src/es/deusto/prog3/cap04/ExploracionRecursiva.java
+++ Clase201920/src/es/deusto/prog3/cap04/ExploracionRecursiva.java
@@ -9,7 +9,70 @@ public class ExploracionRecursiva {
 	
 	public static void main(String[] args) {
 		prueba1();  // Prueba recursiva inicial
+		factorial(); // Prueba factorial
+		factorialv2();
+		prodConSuma();
+		aLaLuna();
 	}
+	
+	private static void aLaLuna() {
+		calcDobleces( 0.00015, 384400000.0, 0 );
+	}
+	
+//	while (grosor>=distancia) {
+//		grosor = grosor*2;
+//      numDobleces = numDobleces+1;
+//	}
+//	System.out.println( numDobleces );
+	private static void calcDobleces( double grosor, double distancia, int numDobleces ) {
+		if (grosor>=distancia) {
+			System.out.println( numDobleces );
+		} else {
+			calcDobleces( grosor*2, distancia, numDobleces+1 );
+		}
+	}
+	
+	public static void prodConSuma() {
+		System.out.println( producto( 6, 5 ));
+	}
+	
+	// Calcula el producto de m y n utilizando solo sumas
+	// Definición:  m*n = m + m*(n-1)           producto(m,n) = m + producto(m,n-1)
+	//              0 si n==0
+	private static int producto( int m, int n ) {
+		if (n==0) {
+			return 0;
+		} else {
+			return m + producto( m, n-1 );
+		}
+	}
+	
+	
+	public static void factorialv2() {
+		fact2( 0, 1, 6 );
+	}
+	
+	private static void fact2( int nInicial, long valorInicial, int nFinal ) {
+		if (nInicial==nFinal) {
+			System.out.println( valorInicial );
+		} else {
+			fact2( nInicial+1, valorInicial*(nInicial+1), nFinal );
+		}
+	}
+	
+	public static void factorial() {
+		System.out.println( fact(21) );
+		System.out.println( Long.MAX_VALUE );
+	}
+	
+	private static long fact(int n) {
+		if (n==0) {
+			return 1;
+		} else {
+			return n * fact(n-1);
+		}
+	}
+	
 
 	// Prueba recursiva inicial
 	private static void prueba1() {
