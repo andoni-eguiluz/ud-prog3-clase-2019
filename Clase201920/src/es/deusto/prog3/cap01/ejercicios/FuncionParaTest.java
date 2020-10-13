d260fc02a59fc5bc4255761a2d858b0e824c1ac5
Andoni Eguíluz <andoni.eguiluz@deusto.es>
2020-10-13 21:49:08 +0200

    Test de función factorial

--- Clase201920/src/es/deusto/prog3/cap01/ejercicios/FuncionParaTest.java
+++ Clase201920/src/es/deusto/prog3/cap01/ejercicios/FuncionParaTest.java
@@ -5,16 +5,22 @@ package es.deusto.prog3.cap01.ejercicios;
  */
 public class FuncionParaTest {
 	
-	/** Devuelve el factorial de un número (producto de ese número por todos los anteriores hasta 1). El factorial de 0 es 1.
+	/** Devuelve el factorial de un número (producto de ese número por todos los anteriores hasta 1).
+	 *  El factorial de 0 es 1.
 	 * @param x	Valor del que calcular el factorial
 	 * @return	Resultado del factorial
 	 * @throws ArithmeticException	Lanzada cuando se intenta un factorial inviable (negativo) o cuando el resultado es incorrecto
 	 */
 	public static int factorial( int x ) throws ArithmeticException {
+		if (x<0) throw new ArithmeticException( "Factorial de negativo es incorrecto" );
+		if (x>12) throw new ArithmeticException( "Factorial no cabe en un int" );  // Posibilidad 1
 		int fact = 1;
 		for (int i=1; i<=x; i++) {
+			long fact2 = (long)fact * i;
 			fact *= i;
-			if (fact<0) throw new ArithmeticException( "Factorial no cabe en un int" );
+			// if (fact<0) throw new ArithmeticException( "Factorial no cabe en un int" );
+			// Otra manera convertir a longs
+			if (fact!=fact2) throw new ArithmeticException( "Factorial no cabe en un int" ); // Posibilidad 2
 		}
 		return fact;
 	}
