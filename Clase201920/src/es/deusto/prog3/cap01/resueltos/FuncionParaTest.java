7a2d1a68c39ee827332e1788f3ab76c1a22ff848
Andoni Eguíluz <andoni.eguiluz@deusto.es>
2020-10-18 17:28:13 +0200

    Test de función factorial con JUnit

--- Clase201920/src/es/deusto/prog3/cap01/resueltos/FuncionParaTest.java
+++ Clase201920/src/es/deusto/prog3/cap01/resueltos/FuncionParaTest.java
@@ -1,11 +1,11 @@
-package es.deusto.prog3.cap01.ejercicios;
+package es.deusto.prog3.cap01.resueltos;
 
 /** Función de ejemplo a testar
  * @author andoni.eguiluz @ ingenieria.deusto.es
  */
 public class FuncionParaTest {
 
-	// TODO - Corregir función con las pruebas
+	// Corregida con las pruebas
 	
 	/** Devuelve el factorial de un número (producto de ese número por todos los anteriores hasta 1).
 	 *  El factorial de 0 es 1.
@@ -14,10 +14,12 @@ public class FuncionParaTest {
 	 * @throws ArithmeticException	Lanzada cuando se intenta un factorial inviable (negativo) o cuando el resultado es incorrecto
 	 */
 	public static int factorial( int x ) throws ArithmeticException {
+		if (x<0) throw new ArithmeticException( "Factorial de negativo es incorrecto" ); // ORIGINALMENTE NO ESTÁ - SE VE CON LAS PRUEBAS 
+		if (x>12) throw new ArithmeticException( "Factorial no cabe en un int" ); // ORIGINALMENTE NO ESTÁ - SE VE CON LAS PRUEBAS 
 		int fact = 1;
 		for (int i=1; i<=x; i++) {
 			fact *= i;
-			if (fact<0) throw new ArithmeticException( "Factorial no cabe en un int" );
+			// ORIGINALMENTE SI ESTABA PERO NO FUNCIONA - SE VE CON LAS PRUEBAS // if (fact<0) throw new ArithmeticException( "Factorial no cabe en un int" );
 		}
 		return fact;
 	}
