6f42a5433be87d621cfbd955c2468b698f2d78d8
Andoni Eguíluz <andoni.eguiluz@deusto.es>
2020-11-11 07:55:52 +0100

    Folio hasta la luna

--- Clase201920/src/es/deusto/prog3/cap04/YLaLuna.java
+++ Clase201920/src/es/deusto/prog3/cap04/YLaLuna.java
@@ -9,6 +9,10 @@ public class YLaLuna {
 		aLaLuna( 0.0001, 384400000.0, 0 );
 	}
 	
+	// Calcula el número de dobleces de un folio hasta la luna de forma recursiva
+	// cálculo (grosor, distancia, num dobleces) -> 
+	//   cálculo( grosor*2, distancia, num dobleces + 1 ) si grosor < distancia
+	//   num dobleces si grosor >= distancia
 	private static void aLaLuna( double grosor, double dist, long numDob ) {
 		if (grosor >= dist) {
 			System.out.println( "Luna encontrada en " + numDob + " dobleces!" );
