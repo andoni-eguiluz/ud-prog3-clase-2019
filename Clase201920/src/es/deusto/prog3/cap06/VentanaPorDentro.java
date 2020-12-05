4aaa56810a8331b234be7001e01272614f2a2d40
Andoni Eguíluz <andoni.eguiluz@deusto.es>
2020-12-05 13:41:54 +0100

    Tema 6

--- Clase201920/src/es/deusto/prog3/cap06/VentanaPorDentro.java
+++ Clase201920/src/es/deusto/prog3/cap06/VentanaPorDentro.java
@@ -8,6 +8,7 @@ import es.deusto.prog3.cap06.pr0506resuelta.gui.VentanaBancoDePruebas;
 public class VentanaPorDentro {
 	public static void main(String[] args) {
 		JFrame f = new JFrame();
+		f.setSize( 400, 300 );
 		JPanel p = new JPanel();
 		p.add( new JButton("1") );
 		p.add( new JButton("2") );
